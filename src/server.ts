644fdc2fd709a08fe07ad9f8d471b9a981282cc8
Preston Arnold <prestonarnold19@gmail.com>
2026-03-06 22:46:25 +0000

    Express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/client";
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serving API on http://localhost:${port}`)
+})
