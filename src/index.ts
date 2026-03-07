11bd19e3edb5434fb584daab799ddaf5e938fd55
Preston Arnold <prestonarnold19@gmail.com>
2026-03-07 08:20:45 +0000

    Ensure default site

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import env from "dotenv"
 import app from "./server";
+import { ensureDefaultSite } from "./services/site";
 
 env.config();
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await ensureDefaultSite();
     console.log(`Serving API on http://localhost:${port}`)
 })
